296646ced468b85e253f55328cdc14a4b2cf3cf2
Soisawan Imwibul <soisawan1540@gmail.com>
2024-10-05 22:54:38 +0700

    initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css',
+    'bootstrap-icons/font/bootstrap-icons.css'
+  ],
+  build: {
+    transpile: ['bootstrap']
+  }
+})
